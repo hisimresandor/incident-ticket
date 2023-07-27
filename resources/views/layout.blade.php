3bc7ef611d0dbd64c6188c599535d5fe50d32760
hisimresandor <his.imre.sandor@gmail.com>
2023-07-27 23:48:00 +0200

    creating index page

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<html lang="hu">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body class="text-bg-light">
+    <div class="text-bg-primary">
+        <div class="row p-2">
+            <div class="col-lg-3"> </div>
+            <div class="col-6">
+                <h4><a href="{{route('index')}}" style="color: inherit;" class="link-underline link-underline-opacity-0">Tech Support</a></h4>
+            </div>
+            <div class="col-lg-3"> </div>
+        </div>
+    </div>
+    <div style="height: 2.5vh;"></div>
+    <div style="margin: auto; width: 750px;" class="p-2">
+        @yield('content')
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+
+@section('title', 'Kezdőlap')
+
+@section('content')
+    <div class="row">
+        <div class="col-6">
+            <a href="#" class="link-underline link-underline-opacity-0">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Hibajegy küldése</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-6">
+            <a href="#" class="link-underline link-underline-opacity-0">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Admin belépés</h5>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+@endsection
